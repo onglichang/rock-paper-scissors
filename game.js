25787ce7aebacd1fff95dfb208d66f130d06456e
onglichang <onglichang@hotmail.com>
2021-11-24 16:52:36 +0800

    Add singleRound of rock paper scissors function

--- game.js
+++ game.js
@@ -10,4 +10,59 @@ function computerPlay() {
     return options[randomChoice];
 }
 
-console.log(computerPlay());
+//console.log(computerPlay());
+
+function singleRound(playerSelection, computerSelection) {
+    // Determines which of the two selection has won the rock paper scissors round 
+    // and returns a string declaring the winner
+
+    // Convert player selection to case insensitive
+    let correctedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
+
+    // Before starting game, confirm if user has inputted a valid input
+    let options = ['Rock', 'Paper', 'Scissors'];
+    if (!(options.includes(correctedPlayerSelection))) {
+        return "Invalid input, try again";
+    }
+
+    // Pre-define a return variable that can be assigned to
+    let declaration;
+
+    // Write all possible comparison cases based on player selection 
+    // 1. All scissor scenarios
+    if (correctedPlayerSelection == 'Scissors') {
+        if (computerSelection == 'Paper') {
+            declaration = "You Win! Scissors beats Paper";
+        } else if (computerSelection == 'Rock') {
+            declaration = "You Lose! Rock beats Scissors";
+        } else {
+            declaration = "Draw!";
+        }
+    } else if (correctedPlayerSelection == 'Paper') {
+        if (computerSelection == 'Rock') {
+            declaration = "You Win! Paper beats Rock";
+        } else if (computerSelection == 'Scissors') {
+            declaration = "You Lose! Scissors beats Paper";
+        } else {
+            declaration = "Draw!";
+        }
+    } else {
+        if (computerSelection == 'Scissors') {
+            declaration = "You Win! Rock beats Scissors";
+        } else if (computerSelection == 'Paper') {
+            declaration = "You Lose! Paper beats Rock";
+        } else {
+            declaration = "Draw!";
+        }
+    }
+
+
+    // Return result as a String declaration
+    return declaration;
+    
+}
+
+
+let comChoice = computerPlay();
+console.log(comChoice);
+console.log(singleRound('PAPER', comChoice));
